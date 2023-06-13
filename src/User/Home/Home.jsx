a0bcbe031970008e2a1d081c975bfffa93d29c54
Uttam Pradhan <[uttampra1@gmail.com]>
2023-06-13 11:12:11 +0545

    Small Changes

--- src/User/Home/Home.jsx
+++ src/User/Home/Home.jsx
@@ -23,25 +23,28 @@ export default function Home() {
     getBlog();
   }, []);
   return (
-    <Box sx={{ width: "80%", margin: "0 auto" }}>
+    <Box sx={{ width: "90%", margin: "0 auto" }}>
       <Typography variant="h3" sx={{ my: 3, textAlign: "Center" }}>
         Blog
       </Typography>
-      <Box sx={{ display: "flex",flexWrap:"wrap",gap:3 }}>
+      <Box sx={{ display: "flex",flexWrap:"wrap",gap:3,justifyContent:"center" }}>
         {blog.map((blog)=>{
           return(
             <Card key={blog.id} sx={{ maxWidth: 345, border: "1px solid black" }}>
           <CardMedia
-            sx={{ height: 140 }}
+            sx={{ height: 140,width:345 }}
             image={blog.Image}
             title="green iguana"
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" sx={{color:"#aaa",fontSize:"16px"}}>
+              {blog.Author}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div" >
               {blog.Title}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-             {blog.Description}
+             {blog.Description.slice(0,40)}
             </Typography>
           </CardContent>
           <CardActions>
