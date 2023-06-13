3364271ac71c58f38048980b9283e1df65ede413
Uttam Pradhan <[uttampra1@gmail.com]>
2023-06-13 08:39:53 +0545

    Adding Card

--- src/User/Home/Home.jsx
+++ src/User/Home/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { baseUrl } from "../../Globals/Config";
-import axios from "axios";
-const Home = () => {
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import {baseUrl} from '../../Globals/Config'
+
+export default function Home() {
   const [blog, setBlog] = React.useState([]);
   const getBlog = async () => {
     let res = await axios.get(`${baseUrl}blog_project`);
@@ -11,7 +18,26 @@ const Home = () => {
   React.useEffect(()=>{
     getBlog();
   },[]);
-  return <div>This is user Home</div>;
-};
-
-export default Home;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
